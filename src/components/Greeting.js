c4763db517c7506678a670ea0f6283ddcb5e1198
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-29 11:13:54 +0000

    first

---
+++ src/components/Greeting.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const Greeting = () => {
+  const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleGreeting = () => {
+    setGreeting(`Hello, ${name}!`);
+  };
+
+  return (
+    <div>
+      <input type="text" value={name} onChange={handleNameChange} placeholder="Enter your name" />
+      <button onClick={handleGreeting}>Greet</button>
+      {greeting && <p>{greeting}</p>}
+    </div>
+  );
+};
+
+export default Greeting;
